595fb49057e7aa1b82aa526c6c25a8d4d9bd514a
Adarsh Kumar <contactadarshk@gmail.com>
2023-08-28 06:46:59 +0530

    add typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { Button } from "./components/ui/button";
+import { TypographyP } from "./components/ui/typography";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      {count}
+      <TypographyP>{count}</TypographyP>
       <Button onClick={() => setCount(count + 1)}>Add</Button>
     </div>
   );

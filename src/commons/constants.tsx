7b9d306d7dcf183b01d96cc8b71bbe7ec83a4025
Adarsh Kumar <contactadarshk@gmail.com>
2023-08-30 01:43:56 +0530

    update endpoint

--- src/commons/constants.tsx
+++ src/commons/constants.tsx
@@ -1,5 +1,8 @@
-export const HOST_PASSPORT = "http://localhost:8081";
-export const HOST_NOMADCORE = "http://localhost:8080";
+// export const HOST_PASSPORT = "http://localhost:8081";
+// export const HOST_NOMADCORE = "http://localhost:8080";
+
+export const HOST_PASSPORT = "https://passport.wanderfusion.com";
+export const HOST_NOMADCORE = "https://api.wanderfusion.com";
 
 export const LOCALSTORAGE_JWT_KEY = "jwt";
 

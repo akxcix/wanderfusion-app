5a48b926f40f3ac8fb6b2eacdc90f3f410d14fed
Adarsh Kumar <contactadarshk@gmail.com>
2023-09-15 03:24:06 +0530

    update hosts

--- src/commons/constants.tsx
+++ src/commons/constants.tsx
@@ -1,12 +1,12 @@
-export const HOSTS = {
-  PASSPORT: "http://localhost:8081",
-  NOMADCORE: "http://localhost:8080",
-};
-
 // export const HOSTS = {
-//   PASSPORT: "https://passport.wanderfusion.com",
-//   NOMADCORE: "https://api.wanderfusion.com",
+//   PASSPORT: "http://localhost:8081",
+//   NOMADCORE: "http://localhost:8080",
 // };
+
+export const HOSTS = {
+  PASSPORT: "https://passport.wanderfusion.com",
+  NOMADCORE: "https://api.wanderfusion.com",
+};
 
 export const LOCAL_STORAGE_KEYS = {
   AUTH_TOKEN: "authToken",
